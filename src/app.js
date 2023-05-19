232438d4f438e6101f43345860271fc77d5c7ddb
nastia-pr <apronyuk@uoc.edu>
2023-05-19 18:20:32 +0200

    added files

---
+++ src/app.js
@@ -0,0 +1,22 @@
+let toggleBtn = document.querySelector(".toggle-btn")
+let toggleBtnIcon = document.querySelector(".toggle-btn i")
+let dropDownMenu = document.querySelector(".dropdown-menu")
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle("open")
+    let isOpen = dropDownMenu.classList.contains("open")
+
+    toggleBtnIcon.classList = isOpen
+        ? "fa-solid fa-xmark"
+        : "fa-solid fa-bars"
+}
+
+
+var map = L.map('map').setView([41.8982,-8.8560], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([41.9011,-8.8746]).addTo(map);
