852e22d7944de54180f7a0e71fb22b1d05a3632b
nastia-pr <apronyuk@uoc.edu>
2023-05-26 00:21:35 +0200

    cambios en el mapa

---
+++ src/mapa.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([41.8982,-8.8560], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([41.9011,-8.8746]).addTo(map);
