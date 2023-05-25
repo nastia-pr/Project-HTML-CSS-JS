b0f10257e97d4f408e182d9b9fdaecd9e5d96493
nastia-pr <apronyuk@uoc.edu>
2023-05-25 18:44:53 +0200

    cambios en el clip-path

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-let toggleBtn = document.querySelector(".toggle-btn")
-let toggleBtnIcon = document.querySelector(".toggle-btn i")
-let dropDownMenu = document.querySelector(".dropdown-menu")
+let toggleBtn = document.querySelector(".toggle-btn");
+let toggleBtnIcon = document.querySelector(".toggle-btn i");
+let dropDownMenu = document.querySelector(".dropdown-menu");
 
 toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle("open")
-    let isOpen = dropDownMenu.classList.contains("open")
+    dropDownMenu.classList.toggle("open");
+    let isOpen = dropDownMenu.classList.contains("open");
 
     toggleBtnIcon.classList = isOpen
         ? "fa-solid fa-xmark"
-        : "fa-solid fa-bars"
-}
+        : "fa-solid fa-bars";
+};
 
 
 var map = L.map('map').setView([41.8982,-8.8560], 12);
@@ -20,3 +20,17 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var marker = L.marker([41.9011,-8.8746]).addTo(map);
+
+
+const containerImg = document.querySelector('.container-img-clip');
+let animationPaused = false;
+
+containerImg.addEventListener('click', function() {
+  if (animationPaused) {
+    containerImg.style.animationPlayState = 'running';
+    animationPaused = false;
+  } else {
+    containerImg.style.animationPlayState = 'paused';
+    animationPaused = true;
+  }
+});
